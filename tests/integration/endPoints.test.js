310953422b799540b33102494af555099d99ca63
muhanzi <muhanagisha@gmail.com>
2021-08-30 01:34:10 +0300

    run some tests

--- tests/integration/endPoints.test.js
+++ tests/integration/endPoints.test.js
@@ -12,26 +12,26 @@ describe("marketing department", () => {
   });
 });
 
-describe("user authentication", () => {
-  // beforeEach()  // may be to clear database before testing or to enter data before testing
-  // Testing the POST method
-  it("tests the post new user endpoint and returns a token", async () => {
-    const response = await supertest(server.appServer)
-      .post("/api/signup")
-      .send({
-        username: "tina",
-        password: "123pass",
-        department: "marketing",
-        role: "employee",
-      });
+// describe("user authentication", () => {
+//   // beforeEach()  // may be to clear database before testing or to enter data before testing
+//   // Testing the POST method
+//   it("tests the post new user endpoint and returns a token", async () => {
+//     const response = await supertest(server.appServer)
+//       .post("/api/signup")
+//       .send({
+//         username: "tina",
+//         password: "123pass",
+//         department: "marketing",
+//         role: "employee",
+//       });
 
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty("token");
-    expect(response.body.message).toBe("signed up successfully");
-  });
+//     expect(response.status).toBe(200);
+//     expect(response.body).toHaveProperty("token");
+//     expect(response.body.message).toBe("signed up successfully");
+//   });
 
-  // This is run after running the test
-  afterEach(async () => {
-    await signUpService.deleteByUsername("tina");
-  });
-});
+//   // This is run after running the test
+//   afterEach(async () => {
+//     await signUpService.deleteByUsername("tina");
+//   });
+// });
